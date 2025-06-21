206154a680456bff00ff7933a2e1323bc6299382
sahanaa21 <sahanaa2060@gmail.com>
2025-06-22 03:17:00 +0530

    err

--- server.js
+++ server.js
@@ -44,11 +44,8 @@ app.get("/", (req, res) => {
 });
 
 // ✅ MongoDB connection + start server
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => {
     console.log("✅ MongoDB Connected");
 
